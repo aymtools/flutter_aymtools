225d0cc0268f6a34b8ec423e7bf795e080514eae
aymtools <anyaming0@gmail.com>
2024-04-07 21:51:02 +0800

    优化项目介绍的结构

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-A related flutter toolkit that is commonly used by individuals. There is no further introduction for
-the time being. Please refer to the relevant source code for specific usage examples.
+A commonly used toolkit in Flutter.
+Intended to make Flutter development more convenient.
+It includes a programmable floating console, some Listenable toolkits, data loaders, convenient
+toast displays, and some commonly used extensions.
 
 ## Usage
+
+### Programmable floating console
+
+### Undoable dialog
+
+### Extension of the association between lifecycle in Flutter and 'cancellable'.
+
+### A dynamically changing loader
+
+### Navigator with interception functionality and related extensions
+
+### A customizable toast similar to Android's
+
+### Other extensions related to 'cancellable' in Flutter
 
 See [example](https://github.com/aymtools/flutter_aymtools/blob/master/example/) for detailed test
 case.
--- lib/src/widgets/widget_ext.dart
+++ lib/src/widgets/widget_ext.dart
@@ -4,10 +4,6 @@ import 'package:aymtools/src/lifecycle/lifecycle_ext.dart';
 import 'package:cancellable/cancellable.dart';
 import 'package:flutter/widgets.dart';
 
-// abstract class ICancellableState<W extends StatefulWidget> {
-//   Cancellable makeCancellable();
-// }
-
 mixin CancellableState<W extends StatefulWidget> on State<W> {
   Cancellable? _base;
 
@@ -80,90 +76,6 @@ mixin CancellableState<W extends StatefulWidget> on State<W> {
   }
 }
 
-// mixin DispatchEventState<W extends StatefulWidget> on State<W>
-//     implements ICancellableState<W> {
-//   Cancellable makeCancellable();
-//
-//   Cancellable? _changeDependenciesCancellable;
-//
-//   Set<void Function(Cancellable cancellable)> _onChangeDependencies = {};
-//
-//   Set<void Function(W widget, W oldWidget)> _onUpdateWidget = {};
-//
-//   void registerOnChangeDependencies(
-//       void Function(Cancellable cancellable) listener) {
-//     if (_changeDependenciesCancellable != null) {
-//       listener(_changeDependenciesCancellable!.makeCancellable());
-//     }
-//     _onChangeDependencies.add(listener);
-//   }
-//
-//   void registerOnUpdateWidget(void Function(W widget, W oldWidget) listener) {
-//     _onUpdateWidget.add(listener);
-//   }
-//
-//   @override
-//   void didChangeDependencies() {
-//     super.didChangeDependencies();
-//
-//     _changeDependenciesCancellable?.cancel();
-//     _changeDependenciesCancellable = makeCancellable();
-//     if (_onChangeDependencies.isEmpty) return;
-//     final listeners = List.of(_onChangeDependencies, growable: false);
-//     for (var l in listeners) {
-//       l(_changeDependenciesCancellable!.makeCancellable());
-//     }
-//   }
-//
-//   @override
-//   void didUpdateWidget(covariant W oldWidget) {
-//     super.didUpdateWidget(oldWidget);
-//     if (_onUpdateWidget.isEmpty) return;
-//     final listeners = List.of(_onUpdateWidget, growable: false);
-//     for (var l in listeners) {
-//       l(widget, oldWidget);
-//     }
-//   }
-//
-//   @override
-//   void dispose() {
-//     _onChangeDependencies.clear();
-//     _onUpdateWidget.clear();
-//     super.dispose();
-//   }
-// }
-//
-// mixin LoaderState<W extends StatefulWidget> on DispatchEventState<W> {
-//   Cancellable? _loading;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     registerOnUpdateWidget((widget, oldWidget) {
-//       if (checkNeedReload(oldWidget)) {
-//         _loading?.cancel();
-//         _reload();
-//       }
-//     });
-//   }
-//
-//   @override
-//   void didChangeDependencies() {
-//     super.didChangeDependencies();
-//     if (_loading == null) {
-//       _reload();
-//     }
-//   }
-//
-//   void _reload() {
-//     _loading = makeCancellable();
-//     loadData(_loading!);
-//   }
-//
-//   bool checkNeedReload(covariant W oldWidget);
-//
-//   void loadData(Cancellable cancellable);
-// }
 extension LazyByRoute on State {
   T lazyByRout<T>() {
     return ModalRoute.of(context)!.settings.arguments as T;
